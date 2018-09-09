50c7370817b4f3cf8c7599fd91ee5a6b3c8844b7
marioacc <mario.acc.15@gmail.com>
2018-09-09 16:59:01 -0400

    Helper methods added.

--- 2.linked_list/LinkedList.js
+++ 2.linked_list/LinkedList.js
@@ -11,6 +11,7 @@ class LinkedList{
     constructor(data){
         this.head = data!== undefined ? new Node(data) : null;
         this.tail = this.head;
+        this.size = this.head ? 1 : 0;
     }
 
     appendToTail(newNodeData) {
@@ -18,27 +19,80 @@ class LinkedList{
         if(this.head === null){
             this.head = newNode;
             this.tail = this.head;
-            return;
+        }else{
+            this.tail.next = newNode;
+            newNode.prev = this.tail;
+            this.tail = newNode;
         }
-           
-        this.tail.next = newNode;
-        newNode.prev = this.tail;
-        this.tail = newNode;
+        this.size++;
         
     }
+    
 
     appendFromArray(array){
         array.forEach(function(e){
             this.appendToTail(e);
-            
+            this.size++
         }.bind(this))
         return this;
+    }
+
+    appendList(linkedList){
+        if(linkedList.head){
+            this.tail.next = linkedList.head;
+            linkedList.head.prev = this.tail;
+            this.size +=linkedList.size;
+            this.tail = linkedList.tail;
+        }
     }
 
     _getTail(){
         return this.tail;
     }
 
+    removeLast(){
+        let oldTail = this.tail;
+        let newTail = this.tail.prev;
+        const oldTailData = oldTail.data;
+        this.tail = newTail; 
+        this.tail.next = null;
+        this.size--;
+        oldTail = null;
+        return oldTailData;
+    }
+
+    removeFirst(){
+        if(this.head){
+            const newHead = this.head.next;
+            if(newHead){
+                newHead.prev = null;
+                this.tail = null;
+            }
+            let oldHead = this.head;
+            const oldHeadData = oldHead.data;
+            this.head = newHead;
+            oldHead = null;
+            this.size--;
+            return oldHeadData
+        }
+        return null;
+    }
+
+    addFirst(data){
+
+        const newHead = new Node(data);
+        if(this.head){
+            this.head.prev = newHead;
+            newHead.next = this.head;
+            this.head = newHead;
+            size++;
+        }else{
+            this.appendToTail(data);
+        }
+        
+        
+    }
+    
     printList(){
         let nodeString='';
         let pointer=this.head;
@@ -48,6 +102,31 @@ class LinkedList{
         }
         nodeString+= pointer.data;
         return nodeString;
+    }
+    // TODO: create method;
+    clone(){
+        let newList =  new LinkedList();
+        let runner =  this.head;
+        while(runner){
+            newList.appendToTail(runner.data);
+            runner = runner.next;
+        }
+
+        return newList;
+    }
+
+    toArray(){
+        const array = [];
+        this._runList(node => array.push(node.data));
+        return array;
+    }
+
+    _runList(cb){
+        let runner = this.head;
+        while (runner) {
+            cb(runner);
+            runner = runner.next;
+        }
     }
 }
 
