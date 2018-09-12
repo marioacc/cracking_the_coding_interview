865768e92ecac68625ad6283ac4ab9373782ab08
marioacc <mario.acc.15@gmail.com>
2018-09-12 08:03:51 -0400

    Documentation and bug fixed.

--- 3.stacks_and_queues/Queue.js
+++ 3.stacks_and_queues/Queue.js
@@ -12,18 +12,27 @@ class Queue {
         this.last = null;
     }
 
+    /**
+     * Add an element to the queue;
+     * @param {data} any 
+     */
     add(data) {
         let newNode = new QueueNode(data);
 
         if (this.last !== null) {
             this.last.next = newNode;
+            this.last = newNode;
         }
-        last = newNode;
-        if (this.first !== null) {
-            this.first = last;
+        else {
+            this.first = newNode;
+            this.last = newNode;
         }
     }
 
+    /**
+     * Remove the first element from the queue;
+     * @return {QueueNode} Last QueueNode of the list; 
+     */
     remove() {
         if (this.first === null)  throw 'Empty Queue';
         let data = this.first.data;
@@ -39,6 +48,10 @@ class Queue {
         return this.first.data;
     }
 
+    /**
+     * Verifies is the Queue is empty
+     * @returns {boolean} false if the queue is empty, true otherwise.
+     */
     isEmpty(){
         return this.first === null;
     }
