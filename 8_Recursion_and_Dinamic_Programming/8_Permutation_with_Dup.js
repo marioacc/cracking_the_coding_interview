0e075b8fb06b8567ca151dc257b8130c8407a375
marioacc <mario.acc.15@gmail.com>
2018-11-28 08:20:30 -0700

    Ex 8 Chapter 8

---
+++ 8_Recursion_and_Dinamic_Programming/8_Permutation_with_Dup.js
@@ -0,0 +1,29 @@
+function printPerms(str) {
+    let result = [];
+    let map = buildFreqTable(str);
+    _printPerms(map, "", str.lenght, result);
+    return result;
+}
+
+function buildFreqTable(s) {
+    let map = {};
+    for (let char of s) {
+        map[char] = map[char] === undefined ? 0 : map[char] + 1;
+    }
+    return map;
+}
+
+function _printPerms(map, prefix, remainig, result) {
+    if (remainig === 0) {
+        result.push(prefix);
+        return;
+    }
+
+    for (let c in map) {
+        let count = map[c];
+        if (count > 0) {
+            map[c] = c - 1;
+            _printPerms(map, prefix + c, remainig - 1, result);
+        }
+    }
+}
